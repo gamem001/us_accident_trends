37edfffb4c5186996645addae7c2ea81ee5e54ee
gamem001 <melgamez@gmail.com>
2020-08-20 21:05:34 -0500

    removing key

--- api_key.py
+++
@@ -1,7 +0,0 @@
-census_key = '26f4835ee9a0944355908ac3c4cef00e1b6ca22e'
-
-# OpenWeatherMap API Key
-weather_key = "0ca76228d7d441107248c6d35691d12b"
-
-# Google API Key
-g_key = "AIzaSyARVJGkPYAIw786z7BTGbVfOcziON44-Zg"
